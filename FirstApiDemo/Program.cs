5f5a792948f36739bce66e8d96fe2b708735fbd8
Burcu BOLAT AKÇA <bbakca@kho.edu.tr>
2024-04-19 16:39:01 +0300

    Rest

--- FirstApiDemo/Program.cs
+++ FirstApiDemo/Program.cs
@@ -16,8 +16,40 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 //Middlewarelar ve sýrasýný buraya yazýyoruz.
 
+//Minimal api yöntemiyle bir api tasarlýyoruz.
+app.MapGet("/", () =>
+{
+    return "Merhaba Dünya";
+}); //get isteði.
+//Bir pattern ve bir delege istiyor
+//Bu get isteðini yaparsak kullanýcýya merhaba müþteri dönecek.
+//Veri text olarak dönecek burada
+//Biz jason olarak dönmesini tercih ederiz.
 
-
+app.MapGet("musteriler", () =>
+{
+    return new[]
+    {
+        new
+        {
+            Id = Guid.NewGuid(),
+            SirketAdi = "SLD Yazýlým",
+            Kisi = "Salih DEMÝROÐ"
+        },
+        new
+        {
+            Id = Guid.NewGuid(),
+            SirketAdi = "CAT Biliþim",
+            Kisi = "Cüneyt TAHMAZ"
+        },
+        new
+        {
+            Id = Guid.NewGuid(),
+            SirketAdi = "Kodlama IO",
+            Kisi = "Engin DEMÝROÐ"
+        }
+    };
+});
 
 
 app.Run();
