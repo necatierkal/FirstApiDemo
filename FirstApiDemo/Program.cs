487f72235ac248cde88c7cc9f3e3041c9edc2d7c
Burcu BOLAT AKÇA <bbakca@kho.edu.tr>
2024-04-22 10:08:34 +0300

    Deneme

--- FirstApiDemo/Program.cs
+++ FirstApiDemo/Program.cs
@@ -26,31 +26,81 @@ app.MapGet("/", () =>
 //Veri text olarak dönecek burada
 //Biz jason olarak dönmesini tercih ederiz.
 
-app.MapGet("musteriler", () =>
-{
-    return new[]
-    {
-        new
-        {
-            Id = Guid.NewGuid(),
-            SirketAdi = "SLD Yazýlým",
-            Kisi = "Salih DEMÝROÐ"
-        },
-        new
-        {
-            Id = Guid.NewGuid(),
-            SirketAdi = "CAT Biliþim",
-            Kisi = "Cüneyt TAHMAZ"
-        },
-        new
-        {
-            Id = Guid.NewGuid(),
-            SirketAdi = "Kodlama IO",
-            Kisi = "Engin DEMÝROÐ"
-        }
-    };
-});
+//app.MapGet("musteriler", () =>
+//{
+//    return new[]
+//    {
+//        new
+//        {
+//            Id = Guid.NewGuid(),
+//            SirketAdi = "SLD Yazýlým",
+//            Kisi = "Salih DEMÝROÐ"
+//        },
+//        new
+//        {
+//            Id = Guid.NewGuid(),
+//            SirketAdi = "CAT Biliþim",
+//            Kisi = "Cüneyt TAHMAZ"
+//        },
+//        new
+//        {
+//            Id = Guid.NewGuid(),
+//            SirketAdi = "Kodlama IO",
+//            Kisi = "Engin DEMÝROÐ"
+//        }
+//    };
+//});
 
+
+//app.MapGet("musteriler/1", () =>
+//{
+//    return new
+//    {
+//        Id = Guid.NewGuid(),
+//        SirketAdi = "SLD Yazýlým",
+//        Kisi = "Salih DEMÝROÐ"
+//    };
+//});
+//app.MapGet("musteriler/2", () =>
+//{
+//    return new
+//    {
+//        Id = Guid.NewGuid(),
+//        SirketAdi = "CAT Biliþim",
+//        Kisi = "Cüneyt TAHMAZ"
+//    };
+//});
+//Id si 1 olan müþteriyi getirir
+//musteriler/1 bir root tanýmý
+//Parametrik tanýmlanabilir.
+
+
+//app.MapGet("musteriler/{id}", (int id) => //id olarak ne gönderirsen gönder hata vermeden çalýþacak. Verdiðin parametre deðerini id olarak alýr.
+////int id deðiþkeninin yazdý çünkü 
+////Bir route a(endpointe) gidildiðinde bir parametre varsa; model binding dediðimiz bir mekanizma devreye girer. Bu model binding bir parametre varsa bu parametreyi set etmeye çalýþýr.
+//// Model binding sýrasýyla form fields, request body, route data, query string parametreleri uploaded files a bakar. Sýradan ilk deðer bulduðunda diðerlerine bakmaz.
+////Bizim örnekte id isimli deðiþken route data da veriliyor. isimlerin eþleþmesi önemli.
+////Bunlara hizmet ya da endpoint denir.
+//{
+//    return new
+//    {
+//        Id = id,
+//        SirketAdi = "SLD Yazýlým",
+//        Kisi = "Salih DEMÝROÐ"
+//    };
+//});
+
+
+
+app.MapGet("musteriler/{id:int}", (int id) => //Burada id parametresine int constrainti ekledik. Artýk badrequest deðil servera gitmeden not found hatasý verir.
+{
+    return new
+    {
+        Id = id,
+        SirketAdi = "SLD Yazýlým",
+        Kisi = "Salih DEMÝROÐ"
+    };
+}); // Orta ve büyük ölçekli projelerde minimal api yazýlmamalý. Bakým maliyeti artar. MVC deseni kullanarak tasarlanmalý.
 
 app.Run();
 
